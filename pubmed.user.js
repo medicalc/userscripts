d8bb93f17725bb7c720a8bf051c1f57a7bba88b8
Simple Medical Calculators <45972793+medicalc@users.noreply.github.com>
2020-05-25 03:35:00 +0200

    Update

--- pubmed.user.js
+++ pubmed.user.js
@@ -6,6 +6,22 @@
 // @include       https://pubmed.ncbi.nlm.nih.gov
 // ==/UserScript==
 
+// function that loads jQuery + callback function
+function addJQuery(callback) {
+  var script = document.createElement("script");
+  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
+  script.addEventListener('load', function() {
+    var script = document.createElement("script");
+    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
+    document.body.appendChild(script);
+  }, false);
+  document.body.appendChild(script);
+}
+
 $(document).ready(function(){
     $('#txt').attr('div.ncbi-alerts', 'colour: lightblue !important');
 });
+
+// load jQuery to execute a given function
+// addJQuery(main);
+
