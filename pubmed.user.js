4ff09e8b1988aef08c9ae3cc0b1e16f18b39f794
Simple Medical Calculators <45972793+medicalc@users.noreply.github.com>
2020-05-25 03:42:52 +0200

    update

--- pubmed.user.js
+++ pubmed.user.js
@@ -7,22 +7,8 @@
 // @include       https://pubmed.ncbi.nlm.nih.gov
 // ==/UserScript==
 
-// function that loads jQuery + callback function
-function addJQuery(callback) {
-  var script = document.createElement("script");
-  script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
-  script.addEventListener('load', function() {
-    var script = document.createElement("script");
-    script.textContent = "window.jQ=jQuery.noConflict(true);(" + callback.toString() + ")();";
-    document.body.appendChild(script);
-  }, false);
-  document.body.appendChild(script);
-}
+var load,execute,loadAndExecute;load=function(a,b,c){var d;d=document.createElement("script"),d.setAttribute("src",a),b!=null&&d.addEventListener("load",b),c!=null&&d.addEventListener("error",c),document.body.appendChild(d);return d},execute=function(a){var b,c;typeof a=="function"?b="("+a+")();":b=a,c=document.createElement("script"),c.textContent=b,document.body.appendChild(c);return c},loadAndExecute=function(a,b){return load(a,function(){return execute(b)})};
 
-$(document).ready(function(){
+loadAndExecute("//ajax.googleapis.com/ajax/libs/jquery/1.6.2/jquery.min.js", function() {
     $('#txt').attr('div.ncbi-alerts', 'colour: lightblue !important');
 });
-
-// load jQuery to execute a given function
-// addJQuery(main);
-
